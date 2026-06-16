055ff22cede612d8a0a66935c9674ab89c93e192
CJ <christianjherardilagan@gmail.com>
2026-06-17 03:06:15 +0800

    template inheritance, used slug for url

--- posts/models.py
+++ posts/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
         ('published', 'Published'),
     ]
 
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
