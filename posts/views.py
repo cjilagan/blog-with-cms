49ea118789eaae9239c538285d4460cc5860454f
CJ <christianjherardilagan@gmail.com>
2026-06-19 00:04:59 +0800

    added pagination

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404 
+from django.core.paginator import Paginator
 from .models import Post
 
 def home(request):
@@ -6,8 +7,11 @@ def home(request):
     return render(request, 'posts/home.html', {'latest_posts': latest_posts})
 
 def post_list(request):
-    posts = Post.objects.filter(status='published')
-    return render(request, 'posts/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(status='published').order_by('-created_at')
+    paginator = Paginator(posts, 5)  
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'posts/post_list.html', {'page_obj': page_obj})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, status='published')
